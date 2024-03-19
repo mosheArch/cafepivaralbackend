ff9c2d2606bc6b118888dc4cbd3155e6008679b7
Alvaro Moises Diaz Diaz <alvaro.moises.diaz.diaz@gmail.com>
2024-03-18 23:02:05 -0700

    Diseño de dasboard del admin

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,19 @@
+import site
+
 from django.contrib import admin
 
-# Register your models here.
+from userauths.models import Profile, User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone_number']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'gender', 'city', 'country']
+    search_fields = ['full_name', 'email', 'phone_number']
+    list_filter = ['date']
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(User, UserAdmin)
--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from shortuuid.django_fields import ShortUUIDField
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    def save(self, *args, **kwargs):
+        emai_username, mobile = self.email.split("@")
+        if self.full_name == "" or self.full_name is None:
+            self.full_name = emai_username
+        if self.username == "" or self.username is None:
+            self.username = emai_username
+
+        super(User, self).save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="image", default="default/default-user.jpg", blank=True, null=True)
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    about_me = models.TextField(max_length=500, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefghijklmnopqrstuvwxyz0123456789")
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name is None:
+            self.full_name = self.user.full_name
+
+        super(Profile, self).save(*args, **kwargs)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
